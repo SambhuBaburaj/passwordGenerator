5d38cded534732412a592c97ceba6828d38403a6
Sambhu Baburaj <106860188+SambhuBaburaj@users.noreply.github.com>
2023-08-25 02:49:31 +0530

    Update Axios.js

--- client/src/API/Axios.js
+++ client/src/API/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 // const baseURL='http://localhost:8000'
-const baseURL='https://password-generatorserver.vercel.app/'
+const baseURL='https://password-generator-server.vercel.app/'
 
 const Instance=axios.create({baseURL})
 
